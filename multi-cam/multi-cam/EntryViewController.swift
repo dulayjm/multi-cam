3cca8cf3178e795b25049567335a331f537d2a0a
dulayjm <dulay.justin19@gmail.com>
2020-12-09 14:42:06 -0600

    Changed template to programatic

---
+++ multi-cam/multi-cam/EntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryViewController.swift
+//  multi-cam
+//
+//  Created by Justin Dulay on 12/9/20.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
