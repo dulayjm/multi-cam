d3d356837b7d254f62fb3a9fd81fa0a57f1ff124
dulayjm <dulay.justin19@gmail.com>
2021-01-12 16:38:18 -0600

    added lidar depth estimator and export

--- multi-cam/multi-cam/AppDelegate.swift
+++ multi-cam/multi-cam/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Justin Dulay on 12/9/20.
 //
 
+import ARKit
 import UIKit
 
 @main
@@ -14,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let supportForSceneReconstruction = ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh)
+        guard supportForSceneReconstruction else {
+            fatalError("Scene Reconstruction isn't supported here")
+        }
+        
         return true
     }
 
---
+++ multi-cam/multi-cam/ViewControllers/LidarViewController.swift
@@ -0,0 +1,341 @@
+//
+//  LidarViewController.swift
+//  multi-cam
+//
+//  Created by Justin Dulay on 1/6/21.
+//
+
+import ARKit
+import Foundation
+import MetalKit
+import ModelIO
+import QuickLook
+import RealityKit
+
+class LidarViewController: UIViewController, ARSessionDelegate {
+    
+    // MARK: - Attributes
+    var arView: ARView = {
+        let view = ARView()
+        return view
+    }()
+    
+    var planeDetectionButton: RoundedButton = {
+        let button = RoundedButton()
+        button.tintColor = .blue
+        button.setTitle("Start", for: .normal)
+        // add target
+        button.addTarget(
+            self,
+            action: #selector(togglePlaneDetectionButtonPressed(_:)),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    var saveButton: RoundedButton = {
+        let button = RoundedButton()
+        button.tintColor = .blue
+        button.setTitle("Save", for: .normal)
+        // add target
+        button.addTarget(
+            self,
+            action: #selector(saveButtonPressed(_:)),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    let coachingOverlay = ARCoachingOverlayView()
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(arView)
+        arView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            arView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            arView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            arView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            arView.heightAnchor.constraint(equalTo: self.view.heightAnchor)
+        ])
+        
+        arView.addSubview(saveButton)
+        arView.addSubview(planeDetectionButton)
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            saveButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 100),
+            saveButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 200),
+            saveButton.widthAnchor.constraint(equalToConstant: 50),
+            saveButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        planeDetectionButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            planeDetectionButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: -100),
+            planeDetectionButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 200),
+            planeDetectionButton.widthAnchor.constraint(equalToConstant: 50),
+            planeDetectionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        arView.session.delegate = self
+        
+        setupCoachingOverlay()
+
+        arView.environment.sceneUnderstanding.options = []
+        
+        // Turn on occlusion from the scene reconstruction's mesh.
+        arView.environment.sceneUnderstanding.options.insert(.occlusion)
+        
+        // Turn on physics for the scene reconstruction's mesh.
+        arView.environment.sceneUnderstanding.options.insert(.physics)
+
+        // Display a debug visualization of the mesh.
+        arView.debugOptions.insert(.showSceneUnderstanding)
+        
+        // For performance, disable render options that are not required for this app.
+        arView.renderOptions = [.disablePersonOcclusion, .disableDepthOfField, .disableMotionBlur]
+        
+        // Manually configure what kind of AR session to run
+        arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.sceneReconstruction = .mesh
+
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Prevent the screen from being dimmed to avoid interrupting the AR experience.
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+    
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    // MARK: - Private Methods
+    
+    /// - Tag: TogglePlaneDetection
+    @objc func togglePlaneDetectionButtonPressed(_ button: UIButton) {
+        guard let configuration = arView.session.configuration as? ARWorldTrackingConfiguration else {
+            return
+        }
+        if configuration.planeDetection == [] {
+            configuration.planeDetection = [.horizontal, .vertical]
+            button.setTitle("Stop Plane Detection", for: [])
+        } else {
+            configuration.planeDetection = []
+            button.setTitle("Start Plane Detection", for: [])
+        }
+        arView.session.run(configuration)
+    }
+    
+    
+    /// - Tag: Export Mesh
+    /// ***********************************************************************************************
+    /// Exporting the LIDAR scan as OBJ file
+    /// - Author: Stefan Pfeifer
+    @objc func saveButtonPressed(_ sender: UIButton) {
+        
+        guard let frame = arView.session.currentFrame else {
+            fatalError("Couldn't get the current ARFrame")
+        }
+        
+        // Fetch the default MTLDevice to initialize a MetalKit buffer allocator with
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Failed to get the system's default Metal device!")
+        }
+        
+        // Using the Model I/O framework to export the scan, so we're initialising an MDLAsset object,
+        // which we can export to a file later, with a buffer allocator
+        let allocator = MTKMeshBufferAllocator(device: device)
+        let asset = MDLAsset(bufferAllocator: allocator)
+        
+        // Fetch all ARMeshAncors
+        let meshAnchors = frame.anchors.compactMap({ $0 as? ARMeshAnchor })
+        
+        // Convert the geometry of each ARMeshAnchor into a MDLMesh and add it to the MDLAsset
+        for meshAncor in meshAnchors {
+            
+            // Some short handles, otherwise stuff will get pretty long in a few lines
+            let geometry = meshAncor.geometry
+            let vertices = geometry.vertices
+            let faces = geometry.faces
+            let verticesPointer = vertices.buffer.contents()
+            let facesPointer = faces.buffer.contents()
+            
+            // Converting each vertex of the geometry from the local space of their ARMeshAnchor to world space
+            for vertexIndex in 0..<vertices.count {
+                
+                // Extracting the current vertex with an extension method provided by Apple in Extensions.swift
+                let vertex = geometry.vertex(at: UInt32(vertexIndex))
+                
+                // Building a transform matrix with only the vertex position
+                // and apply the mesh anchors transform to convert into world space
+                var vertexLocalTransform = matrix_identity_float4x4
+                vertexLocalTransform.columns.3 = SIMD4<Float>(x: vertex.0, y: vertex.1, z: vertex.2, w: 1)
+                let vertexWorldPosition = (meshAncor.transform * vertexLocalTransform).position
+                
+                // Writing the world space vertex back into it's position in the vertex buffer
+                let vertexOffset = vertices.offset + vertices.stride * vertexIndex
+                let componentStride = vertices.stride / 3
+                verticesPointer.storeBytes(of: vertexWorldPosition.x, toByteOffset: vertexOffset, as: Float.self)
+                verticesPointer.storeBytes(of: vertexWorldPosition.y, toByteOffset: vertexOffset + componentStride, as: Float.self)
+                verticesPointer.storeBytes(of: vertexWorldPosition.z, toByteOffset: vertexOffset + (2 * componentStride), as: Float.self)
+            }
+            
+            // Initializing MDLMeshBuffers with the content of the vertex and face MTLBuffers
+            let byteCountVertices = vertices.count * vertices.stride
+            let byteCountFaces = faces.count * faces.indexCountPerPrimitive * faces.bytesPerIndex
+            let vertexBuffer = allocator.newBuffer(with: Data(bytesNoCopy: verticesPointer, count: byteCountVertices, deallocator: .none), type: .vertex)
+            let indexBuffer = allocator.newBuffer(with: Data(bytesNoCopy: facesPointer, count: byteCountFaces, deallocator: .none), type: .index)
+            
+            // Creating a MDLSubMesh with the index buffer and a generic material
+            let indexCount = faces.count * faces.indexCountPerPrimitive
+            let material = MDLMaterial(name: "mat1", scatteringFunction: MDLPhysicallyPlausibleScatteringFunction())
+            let submesh = MDLSubmesh(indexBuffer: indexBuffer, indexCount: indexCount, indexType: .uInt32, geometryType: .triangles, material: material)
+            
+            // Creating a MDLVertexDescriptor to describe the memory layout of the mesh
+            let vertexFormat = MTKModelIOVertexFormatFromMetal(vertices.format)
+            let vertexDescriptor = MDLVertexDescriptor()
+            vertexDescriptor.attributes[0] = MDLVertexAttribute(name: MDLVertexAttributePosition, format: vertexFormat, offset: 0, bufferIndex: 0)
+            vertexDescriptor.layouts[0] = MDLVertexBufferLayout(stride: meshAncor.geometry.vertices.stride)
+            
+            // Finally creating the MDLMesh and adding it to the MDLAsset
+            let mesh = MDLMesh(vertexBuffer: vertexBuffer, vertexCount: meshAncor.geometry.vertices.count, descriptor: vertexDescriptor, submeshes: [submesh])
+            asset.add(mesh)
+        }
+        
+        // Setting the path to export the OBJ file to
+        let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let urlOBJ = documentsPath.appendingPathComponent("scan.obj")
+
+        // Exporting the OBJ file
+        if MDLAsset.canExportFileExtension("obj") {
+            do {
+                try asset.export(to: urlOBJ)
+                
+                // Sharing the OBJ file
+                let activityController = UIActivityViewController(activityItems: [urlOBJ], applicationActivities: nil)
+                activityController.popoverPresentationController?.sourceView = sender
+                self.present(activityController, animated: true, completion: nil)
+            } catch let error {
+                fatalError(error.localizedDescription)
+            }
+        } else {
+            fatalError("Can't export OBJ")
+        }
+    }
+    /// ***********************************************************************************************
+
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        DispatchQueue.main.async {
+            // Present an alert informing about the error that has occurred.
+            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+                alertController.dismiss(animated: true, completion: nil)
+            }
+            alertController.addAction(restartAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+}
+
+extension LidarViewController: ARCoachingOverlayViewDelegate {
+    
+    func coachingOverlayViewWillActivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        planeDetectionButton.isHidden = true
+        saveButton.isHidden = true
+    }
+
+    func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+        planeDetectionButton.isHidden = false
+        saveButton.isHidden = false
+    }
+
+    func setupCoachingOverlay() {
+        // Set up coaching view
+        coachingOverlay.session = arView.session
+        coachingOverlay.delegate = self
+        
+        coachingOverlay.translatesAutoresizingMaskIntoConstraints = false
+        arView.addSubview(coachingOverlay)
+        
+        NSLayoutConstraint.activate([
+            coachingOverlay.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            coachingOverlay.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            coachingOverlay.widthAnchor.constraint(equalTo: view.widthAnchor),
+            coachingOverlay.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+    }
+}
+
+extension simd_float4x4 {
+    var position: SIMD3<Float> {
+        return SIMD3<Float>(columns.3.x, columns.3.y, columns.3.z)
+    }
+}
+
+extension Transform {
+    static func * (left: Transform, right: Transform) -> Transform {
+        return Transform(matrix: simd_mul(left.matrix, right.matrix))
+    }
+}
+
+extension ARMeshGeometry {
+    func vertex(at index: UInt32) -> (Float, Float, Float) {
+        assert(vertices.format == MTLVertexFormat.float3, "Expected three floats (twelve bytes) per vertex.")
+        let vertexPointer = vertices.buffer.contents().advanced(by: vertices.offset + (vertices.stride * Int(index)))
+        let vertex = vertexPointer.assumingMemoryBound(to: (Float, Float, Float).self).pointee
+        return vertex
+    }
+    
+}
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        backgroundColor = tintColor
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        setTitleColor(.white, for: [])
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+    }
+    
+    override var isEnabled: Bool {
+        didSet {
+            backgroundColor = isEnabled ? tintColor : .gray
+        }
+    }
+}
+
