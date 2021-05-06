452047ab2c22b5054e8b5949a1bdda2bc49c213a
Justin Dulay <43012948+dulayjm@users.noreply.github.com>
2021-05-06 09:31:22 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import numpy as np
+import os
 import cv2
+import json
+from PIL import Image
 
 # Initialize the Flask application
 app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def health(): 
-    response = {'message': 'health check up'}
+    response = {'okay!': 'health check up'}
     return Response(response=response, status=200, mimetype="application/json")
 
 # route http posts to this method
@@ -15,22 +18,51 @@ def health():
 def test():
     r = request
     # convert string of image data to uint8
-    nparr = np.fromstring(r.data, np.uint8)
-    # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    print("The image is, ", img)
-    # do some fancy processing here...
 
-    # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-                }
+    # print("um the request is:", request.data)
 
-    print("the response is: ", response)
-    # maybe have to encode the response ...
-
-    return Response(response=response, status=200, mimetype="application/json")
+    f = request.files
+    print("files" , f)
+    a = request.args
+    print("args", a)
+    j = request.json
+    print("json", j)
 
 
-# start flask app
+    img = request.args.get('img.jpeg')
+
+
+    # files = request.files.getlist('file[]')
+
+    # print("files are", files)
+    # print('length of files are ', len(files))
+
+    # for file in files: 
+
+    # Read the image via file.stream
+    # img = Image.open(file.stream)
+    im = np.asarray(img)
+    print("the image image array of np of the image is: ", im)
+
+    # write img to file, do some DL function etc ...
+    print('here')
+    saveImageToFolder(im)
+
+    response = {'message': 'image received. size='}
+    return jsonify(response)
+
+def saveImageToFolder(img): 
+    save_path = '/Users/justindulay/Downloads/'
+    file_name = "img-test.png"
+    completeName = os.path.join(save_path, file_name)
+
+    print("the complete name is ", completeName)
+
+    cv2.imwrite(completeName, img)
+
+def doSomeComputerVisionThing(): 
+    pass
+
+# # start flask app
 if __name__ == "__main__":
-    app.run(host="192.168.1.109", port=5000)
+    app.run(host="192.168.1.109", port=3000)
